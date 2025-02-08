4f217a046a2fbed3f1c882157595fe35b4d92312
Creeford Sanchez <creewrk@gmail.com>
2025-02-08 15:57:43 -0800

    add-header-transition

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,25 @@
 "use strict";
 
+function Listen(selector, event, callback) {
+    return selector.addEventListener(event, callback);
+}
+
+function Select(selector) {
+    return document.querySelector(selector);
+}
+
+function SelectAll(selector) {
+    return document.querySelectorAll(selector);
+}
+
 // Header fixed
+const header = Select('header');
+
+Listen(window, 'scroll', () => {
+    let position = window.scrollY;
+    if (position > 70) {
+        header.classList.add('header-fixed');
+    } else {
+        header.classList.remove('header-fixed');
+    }
+});
