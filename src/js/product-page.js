e87c4bbf8f045ea18a86f04eb115566aa312e5e7
Elijah Gabrielle Tanabe <elijahgabriellet@gmail.com>
2025-02-15 03:21:45 -0600

    Created modal design
    CHANGES:
    - First iteration of modal design
    - Created new css file for modal
    - Connected product-item design to modal with js
    TODO:
    - Revaluate design
    - New product-list design

--- src/js/product-page.js
+++ src/js/product-page.js
@@ -2,6 +2,19 @@ const openButton = document.querySelectorAll(".product-preview-button");
 const closeButton = document.querySelector(".modal-close-button");
 const modal = document.querySelector(".modal-container");
 const modalOverlay = document.querySelector(".modal-overlay");
+const modalImage = document.querySelector(".modal-image");
+const modalTitle = document.querySelector(".modal-title");
+const modalPrice = document.querySelector(".modal-price");
+const modalWeight = document.querySelector(".modal-weight");
+const modalDescription = document.querySelector(".modal-description");
+
+const productImage = document.querySelectorAll(".product-image");
+const productName = document.querySelectorAll(".product-name p");
+const productPrice = document.querySelectorAll(".product-price");
+const productServing = document.querySelectorAll(".product-serving");
+const productDescription = document.querySelectorAll(".product-hidden-description");
+
+const productItems = document.querySelectorAll(".product-item");
 
 closeButton.addEventListener("click", () => {
     modal.classList.add("modal-disappear-opacity");
@@ -16,8 +29,13 @@ closeButton.addEventListener("click", () => {
     }, 200);
 });
 
-openButton.forEach(button => {
+openButton.forEach((button, index) => {
     button.addEventListener("click", () => {
+        modalImage.src = productImage[index].getAttribute("src");
+        modalTitle.innerText = productName[index].innerText;
+        modalPrice.innerText = productPrice[index].innerText;
+        modalWeight.innerText = productServing[index].innerText;
+        modalDescription.innerText = productDescription[index].innerText;
         modal.classList.add("appear-modal");
         modal.classList.remove("disappear-modal");
         modalOverlay.classList.add("appear-modal");
